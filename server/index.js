f0b34a32a66a03d0d935fba446d7cf27b7cc2580
Krishnakumar V <krishkumarv2001@gmail.com>
2023-09-14 13:12:03 +0530

    vercel deploy server: config files#3

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import path from "path";
+// import path from "path";
 
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
@@ -26,19 +26,19 @@ app.use(allowCrossDomain);
 app.use("/api/users", userRoutes);
 
 // --------------------------deployment------------------------------
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
